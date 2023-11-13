748df849cd310f08b6144360b33844b958c19c2b
ChenZakaim <chenzakaim@hilma.tech>
2023-11-13 16:18:46 +0200

    added users

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import GameFooter from "./components/GameFooter";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([/*localstorage*/]);
+  /////
+  function startGame() {}//!!
+  /////
+  function addNewPlayer(name) {
+    const newUser = {name, averageScore:0, gameCount:0};
+    setUsers([...users, newUser]);
+  }
   return (
     <>
       <h1> First To 100 </h1>
       <Game />
-      <GameFooter />
+      <GameFooter addNewPlayer={addNewPlayer} startGame={startGame} />
     </>
   );
 }

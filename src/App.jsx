13be75df2b76fa230f9742f1f7c5331bf2fd4a8b
EyalPaz-700 <eyalp700@gmail.com>
2023-11-13 16:16:19 +0200

    game 1st version

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import GameFooter from "./components/GameFooter";
 import "./App.css";
 
 function App() {
+  const [players, setPlayers] = useState([
+    {
+      name: "eyal",
+    },
+    {
+      name: "shlomo",
+    },
+    {
+      name: "paz",
+    },
+  ]);
+
+  function removePlayer(name) {
+    setPlayers((prev) => {
+      return prev.filter((el) => el.name !== name);
+    });
+  }
   return (
     <>
       <h1> First To 100 </h1>
-      <Game />
+      <Game removeUser={removePlayer} players={players} />
       <GameFooter />
     </>
   );
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/game.css";
 import GameCard from "./GameCard";
-export default function Game() {
+export default function Game({ players, removeUser }) {
+  const [currentTurn, setCurrentTurn] = useState(0);
+  function changeTurn() {
+    if (currentTurn >= players.length - 1) {
+      setCurrentTurn(0);
+    } else {
+      setCurrentTurn((prev) => prev + 1);
+    }
+  }
   return (
     <div className="game-container">
-      <GameCard />
-
-      <GameCard />
+      {currentTurn}
+      {players.map((player, index) => {
+        return (
+          <GameCard
+            removeUser={() => {
+              if (currentTurn > index) {
+                setCurrentTurn((prev) => prev - 1);
+              }
+              removeUser(player.name);
+            }}
+            changeTurn={changeTurn}
+            currentTurn={currentTurn}
+            name={player.name}
+            myIndex={index}
+            key={index}
+          />
+        );
+      })}
     </div>
   );
 }

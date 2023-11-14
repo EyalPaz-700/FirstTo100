b4446750481da89a8a860824211d4d906e6aa412
ChenZakaim <chenzakaim@hilma.tech>
2023-11-14 11:58:27 +0200

    hhh

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,8 @@ function App() {
     return copy;
   }
   const [players, setPlayers] = useState(
-    changePlayersStatus(JSON.parse(localStorage.getItem("players"))) || []
+    // changePlayersStatus(JSON.parse(localStorage.getItem("players"))) || []
+    []
   );
   const [gameStatus, setGameStatus] = useState(false);
 
@@ -34,22 +35,20 @@ function App() {
     const newUser = { name, averageScore: 0, gameCount: 0, didWin: false };
     const newGamePlayers = [...players, newUser];
     const oldPlayers = changePlayersStatus(JSON.parse(localStorage.getItem("players"))) || [];
-    console.log('newUser: ', newUser);
-    console.log('newGamePlayers: ', newGamePlayers);
-    console.log('oldPlayers: ', oldPlayers);
-    
+   
+    let playerFound= false;
     const allPlayers = oldPlayers.forEach(element => {
-      
         if (element.name ===newGamePlayers[newGamePlayers.length-1].name) {
-          console.log('newGamePlayers[i]: ', newGamePlayers[i]);
-          console.log('element: ', element);
-          newGamePlayers[i].averageScore = element.averageScore;
-          newGamePlayers[i].gameCount = gameCount;
+          playerFound = true;
+          newGamePlayers[newGamePlayers.length-1].averageScore = element.averageScore;
+          newGamePlayers[newGamePlayers.length-1].gameCount = element.gameCount;
         }
-      
     });
+    if(!playerFound){
+      oldPlayers.push(newUser);
+    }
     setPlayers(allPlayers || newGamePlayers);
-    localStorage.setItem("players", JSON.stringify(allPlayers || newGamePlayers));
+    localStorage.setItem("players", JSON.stringify(oldPlayers));
   }
 
   function changeWinStatus(userName, score) {
@@ -61,7 +60,7 @@ function App() {
         (result.averageScore * result.gameCount + score) /
         (result.gameCount + 1);
       result.gameCount++;
-      localStorage.setItem("players", JSON.stringify(copy));
+      // localStorage.setItem("players", JSON.stringify(copy));
       return copy;
     });
   }

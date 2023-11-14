cd1cd72605f92608f5635dd291ea268d00c7a76e
ChenZakaim <chenzakaim@hilma.tech>
2023-11-14 13:17:35 +0200

    fixed local storage issues

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import GameFooter from "./components/GameFooter";
 import "./App.css";
 
 function App() {
+  const [players, setPlayers] = useState(
+    // changePlayersStatus(JSON.parse(localStorage.getItem("players"))) || []
+    []
+  );
+  const [gameStatus, setGameStatus] = useState(false);
+
   function removePlayer(name) {
     setPlayers((prev) => {
       return prev.filter((el) => el.name !== name);
@@ -11,21 +17,42 @@ function App() {
   }
   function changePlayersStatus(playersArr) {
     const copy = [...playersArr];
-    copy.map((elm)=>{
-      elm.didWin = false;
-      return elm;
-    });
+    if (copy.length > 0) {
+      copy.map((elm) => {
+        if (typeof elm.didWin === "boolean") {
+          elm.didWin = false;
+        }
+        return elm;
+      });
+    }
     return copy;
   }
-  const [players, setPlayers] = useState(
-    // changePlayersStatus(JSON.parse(localStorage.getItem("players"))) || []
-    []
-  );
-  const [gameStatus, setGameStatus] = useState(false);
-
+  function updatePlayersInStorage() {
+    const oldPlayers = changePlayersStatus(
+      JSON.parse(localStorage.getItem("players")) || []
+    );
+    localStorage.setItem(
+      "players",
+      JSON.stringify(
+        oldPlayers.map((fromStorage) => {
+          let playerFound = false;
+          for (let player of players) {
+            if (fromStorage.name === player.name) {
+              playerFound = true;
+              return player;
+            }
+          }
+          if (!playerFound) {
+            return fromStorage;
+          }
+        })
+      )
+    );
+  }
   function toogleGameMode() {
     setGameStatus((prev) => {
       if (prev) {
+        updatePlayersInStorage();
         setPlayers([]);
       }
       return !prev;
@@ -34,17 +61,20 @@ function App() {
   function addNewPlayer(name) {
     const newUser = { name, averageScore: 0, gameCount: 0, didWin: false };
     const newGamePlayers = [...players, newUser];
-    const oldPlayers = changePlayersStatus(JSON.parse(localStorage.getItem("players"))) || [];
-   
-    let playerFound= false;
-    const allPlayers = oldPlayers.forEach(element => {
-        if (element.name ===newGamePlayers[newGamePlayers.length-1].name) {
-          playerFound = true;
-          newGamePlayers[newGamePlayers.length-1].averageScore = element.averageScore;
-          newGamePlayers[newGamePlayers.length-1].gameCount = element.gameCount;
-        }
+    const oldPlayers = changePlayersStatus(
+      JSON.parse(localStorage.getItem("players")) || []
+    );
+
+    let playerFound = false;
+    const allPlayers = oldPlayers.forEach((element) => {
+      if (element.name === newGamePlayers[newGamePlayers.length - 1].name) {
+        playerFound = true;
+        newGamePlayers[newGamePlayers.length - 1].averageScore =
+          element.averageScore;
+        newGamePlayers[newGamePlayers.length - 1].gameCount = element.gameCount;
+      }
     });
-    if(!playerFound){
+    if (!playerFound) {
       oldPlayers.push(newUser);
     }
     setPlayers(allPlayers || newGamePlayers);
@@ -78,6 +108,8 @@ function App() {
         addNewPlayer={addNewPlayer}
         toogleGameMode={toogleGameMode}
         gameStatus={gameStatus}
+        changePlayersStatus={changePlayersStatus}
+        players={players}
       />
     </>
   );
